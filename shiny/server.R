ee997d0fb52eb3c2d0e7acd9029e7e5cdd7c9ebe
Zoë Wilkinson Saldaña <zoews@umich.edu>
2019-04-30 22:08:21 -0400

    added variable selection via UI, box plots, more hover text

--- shiny/server.R
+++ shiny/server.R
@@ -3,6 +3,7 @@ library(googledrive)
 library(googlesheets4)
 library(ggthemr)
 library(GGally)
+# library(ggwordcloud)
 
 function(input, output, session) {
   
@@ -10,6 +11,7 @@ function(input, output, session) {
   
   interaction_type <- "none"
   
+
   observeEvent(input$user_click, print("click"))
   observeEvent(input$user_brush, print("brush"))
   observeEvent(input$user_hover, print("hover"))
@@ -27,7 +29,7 @@ function(input, output, session) {
 mood_data_full <- reactiveValues(data=setNames(data.frame(matrix(ncol = 4, nrow = 0)), c("Date", "Mood", "High", "Low")))
 
 #stores full or subset of data to generate plots
-mood_data_to_plot <- reactiveValues(data=setNames(data.frame(matrix(ncol = 4, nrow = 0)), c("Date", "Mood", "High", "Low")))
+mood_data_to_plot <- reactiveValues(data=setNames(data.frame(matrix(ncol = 4, nrow = 0)), c("Date", "Mood", "High", "Low")), natureIndicator = "Mood")
     
 observeEvent(input$go, {
   data_sheet_handle <-drive_get("mood_journal")
@@ -35,14 +37,25 @@ observeEvent(input$go, {
   mood_data <- read_sheet(data_sheet_handle)
   
   mood_data <- mood_data %>%
-    mutate(Date = as.Date(Date))
+    mutate(Date = as.Date(Date)) %>%
+    mutate(Any_Exercise = (Exercise > 0)) %>%
+    mutate(Mood_Range = High - Low) %>%
+    select(Date, Mood, High, Low, Mood_Range, Exercise, Sleep, Nature, Walk, Internet, Any_Exercise, Notes, Grateful)
   
-  mood_data <- mood_data %>%
-    mutate(Any_Exercise = (Exercise > 0))
+  
   
   
   mood_data_full$data <- mood_data
   mood_data_to_plot$data <- mood_data
+})
+
+observeEvent(input$avg, {
+  mood_data_to_plot$data <- mood_data_to_plot$data %>%
+    mutate(Mood = (High + Low)/2)
+})
+
+observeEvent(input$walk_nature_indicator, {
+  mood_data_to_plot$natureIndicator <- input$walk_nature_indicator
 })
 
 
@@ -61,12 +74,36 @@ output$big_mood_plot <- renderPlot({
 }, execOnResize = TRUE)
 
 output$a_new_plot <- renderPlot({
-  ggcorr(mood_data_to_plot$data)
+  ggcorr(mood_data_to_plot$data) +
+    theme(text = element_text(size=16))
 })
 
+output$walk_and_nature <- renderPlot({
+  boxplot_data <- mood_data_to_plot$data %>%
+    mutate (Walk_and_Nature = paste(Walk, "Walk,", Nature, "Nature"))
+  
+  
+  ggplot(boxplot_data, aes_string(x = "Walk_and_Nature", y = mood_data_to_plot$natureIndicator)) +
+    theme(text = element_text(size=16), 
+          axis.text.x = element_text(angle = 30, hjust = 1)) +
+    geom_boxplot()
+  
+})
+
+# output$gratitude_plot <- renderPlot({
+#   set.seed(42)
+#   
+#   word_data <- mood_data_to_plot$data %>%
+#     mutate(Grateful =  strsplit(gsub('[[:punct:] ]+',' ',Grateful), ' '))
+#   
+#   ggplot(word_data, aes(label = Grateful)) +
+#     geom_text_wordcloud()
+# 
+# })
 output$info <- renderPrint({
   out <- nearPoints(mood_data_to_plot$data, input$user_hover, xvar = "Date", yvar = "Mood", threshold = 10, maxpoints = 1, addDist = TRUE)
-  if(nrow(out) > 0) out$Notes
+  if(nrow(out) > 0) paste(out$Notes,
+                          '<br ><br >Sleep: ', out$Sleep, 'hours<br>Nature: ', out$Nature, '<br>Walk: ', out$Walk, '<br>Exercise:', out$Exercise, 'minutes<br>Internet: ', out$Internet, 'minutes')
 })
 
   
--- shiny/ui.R
+++ shiny/ui.R
@@ -6,21 +6,40 @@ fluidPage(theme = shinytheme("readable"),
     textInput("mood_sheet_name", "Your journal name (in Drive)", "mood_journal"),
     actionButton("go", "Load data"),
     hr(),
+    br(),
+    strong('Override mood variable?'),
+    br(),
+    actionButton("avg", "Yes, use mean"),
+    br(),
+    hr(),
     dateRangeInput("daterange1", "Date range:",
                    start = "2019-04-01",
                    end   = Sys.Date() + 1),
-    hr()
+    hr(),
+    selectInput("walk_nature_indicator", "Walk & Nature Indicator:",
+                c("Overall mood" = "Mood",
+                  "High mood" = "High",
+                  "Low mood" = "Low"))
   ),
   mainPanel(
     br(),
     h4('Changes in mood over time (overall, high, and low per day)'),
     plotOutput('big_mood_plot', click='user_click', brush = 'user_brush', hover = 'user_hover'),
     br(),
-    textOutput("info"),
+    p(htmlOutput("info")),
     
     hr(),
     h4('Correlation matrix for mood data'),
     plotOutput('a_new_plot'),
+    br(),
+    h4('Mood distribution by walk and nature experiences'),
+    plotOutput('walk_and_nature'),
+    br(),
+    # hr(),
+    # 
+    # br(),
+    # h4('Gratitude words...!'),
+    # plotOutput('gratitude_plot'),
     br()
   )
 )
